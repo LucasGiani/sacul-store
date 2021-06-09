9d0218313f2c995dffcfd16b6fc38366b8d1f7df
LucasGiani <LucasGiani_05@outlook.com.ar>
2021-06-08 21:23:50 -0300

    Desafío: Menú e-commerce

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (<>
+    <NavBar />
+    
+    <h2>Las ofertas de la semana</h2>
+  </>
+  );
+}
+
+export default App;
