24581acc0c734f4b7c29864979cdd8edb1162746
LucasGiani <LucasGiani_05@outlook.com.ar>
2021-06-21 23:10:25 -0300

    Eliminación de HomeContainer innecesario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HomeContainer } from './containers/HomeContainer';
+import { useState } from "react"
+import { NavBar } from "./components/NavBar"
+import { ItemListContainer } from "./containers/ItemListContainer"
 
 
 function App() {
+  const [cart, setCart] = useState([]);
+
   return (
-    <HomeContainer/>
+    <section>
+      <NavBar cart={cart}/>
+      <ItemListContainer onAdd={(producto) => setCart([...cart, producto])} greeting={'Hola, te damos la bienvenida a nuestro e-commerce!'}/>
+    </section>
   );
 }
 
